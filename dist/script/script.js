eaec19d6b2fdbd5230a68345afc988f37eb30031
halil_95 <halilaliyew@gmail.com>
2024-04-28 21:17:53 +0300

    change

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,1 +1,32 @@
-// some code 
+// Scroll up button
+const scrollBtn = document.querySelector(".scroll-up");
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const bars = document.querySelector(".fa-bars");
+const xMark = document.querySelector(".fa-xmark");
+const navList = document.querySelectorAll(".nav-list");
+
+// Scroll up button
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 100) {
+    scrollBtn.classList.add("top");
+  } else {
+    scrollBtn.classList.remove("top");
+  }
+};
+
+// show menu
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  bars.classList.toggle("active");
+  xMark.classList.toggle("active");
+});
+
+// hide menu
+navList.forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    bars.classList.toggle("active");
+    xMark.classList.toggle("active");
+  });
+});
